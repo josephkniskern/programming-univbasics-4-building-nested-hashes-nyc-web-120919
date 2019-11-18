b359d2026e14b110465ba27ebd3728c1aec75af1
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-18 23:30:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,9 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4, 
       :rent_in_dollars => {
-        :owner1 => 1,
-        :owner2 => 2,
-        :owner3 => 3,
+        :one_piece_owned => 25,
+        :two_piece_owned => 50,
+        :three_piece_owned => 100,
         :owner4 => 4
       },
       :names => {
