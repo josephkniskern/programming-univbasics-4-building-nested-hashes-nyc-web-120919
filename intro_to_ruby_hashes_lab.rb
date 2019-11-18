1ba490842f29fcaf37b00bc415fb23ef00d575ef
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-18 23:30:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
         :one_piece_owned => 25,
         :two_piece_owned => 50,
         :three_piece_owned => 100,
-        :owner4 => 4
+        :four_pieces_owned => 200
       },
       :names => {
         :name1 => "Jeff",
